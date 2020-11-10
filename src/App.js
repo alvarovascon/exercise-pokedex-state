af25a9ac9db63b28f404a34116145dcef91600fb
alvaro <alvarovascon2@gmail.com>
2020-11-10 18:04:48 -0300

    starting Pokedex exercise

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-import Pokedex from './Pokedex';
+import Pokedex from './pokedex';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return(
     <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <h1>Pokedex</h1>
+      <Pokedex pokemons={pokemons}/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
